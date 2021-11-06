7db41029282983323a2a79850db94e19ccfde3fa
ddojai <favianred@gmail.com>
2021-11-06 23:31:02 +0900

    Init project

---
+++ src/main/java/io/github/ddojai/springboot/reactive/SpringbootReactiveApplication.java
@@ -0,0 +1,13 @@
+package io.github.ddojai.springboot.reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootReactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootReactiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/ddojai/springboot/reactive/SpringbootReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.ddojai.springboot.reactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootReactiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
